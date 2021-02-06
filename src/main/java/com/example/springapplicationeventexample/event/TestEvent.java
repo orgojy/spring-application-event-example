48d894296fb648807b815d0c0990fafb080c5dae
orgojy <giveawesome@gmail.com>
2021-02-06 14:27:14 +0900

    feat(TestEvent.java): event message for ApplicationEvent

---
+++ src/main/java/com/example/springapplicationeventexample/event/TestEvent.java
@@ -0,0 +1,16 @@
+package com.example.springapplicationeventexample.event;
+
+import org.springframework.context.ApplicationEvent;
+
+public class TestEvent extends ApplicationEvent {
+    private final String message;
+
+    public TestEvent(Object source, String message) {
+        super(source);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
