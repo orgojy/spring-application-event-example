78858169f1d5b58d78201e43db999e3222bf86e8
orgojy <giveawesome@gmail.com>
2021-02-06 13:46:47 +0900

    feat: basic settings for example

---
+++ src/main/java/com/example/springapplicationeventexample/SpringApplicationEventExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapplicationeventexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationEventExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationEventExampleApplication.class, args);
+	}
+
+}
