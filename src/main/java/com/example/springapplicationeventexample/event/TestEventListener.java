609e5efe9bdaf6a17d6d0a21908471e9d6f04caa
orgojy <giveawesome@gmail.com>
2021-02-08 21:40:23 +0900

    feat: listener of application event for @TransactionalEventListener

--- src/main/java/com/example/springapplicationeventexample/event/TestEventListener.java
+++ src/main/java/com/example/springapplicationeventexample/event/TestEventListener.java
@@ -3,12 +3,19 @@ package com.example.springapplicationeventexample.event;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class TestEventListener {
     @Async
     @EventListener
     public void handleTestEvent(TestEvent event) {
+        System.out.println(event.getMessage());
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleTestEventWithoutFailedTransaction(TestEvent event) {
         System.out.println(event.getMessage());
     }
 }
